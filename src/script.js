c5912745d80ae1ecaf646df3f3194ff6b7b01842
jtycode <95986369+jtycode@users.noreply.github.com>
2021-12-17 23:03:52 -0500

    Updated forecast forEach function
    forecast now shows the next coming 5 day forecast.
    (i.e. today: F
           forecast: Sa, Su, M, T, W)
    
    previously showed current day in the forecast
    (i.e. today: Fri
          forecast: F, Sa, Su, M, T)

--- src/script.js
+++ src/script.js
@@ -46,7 +46,7 @@ function displayForecast(response) {
   let forecastHTML = `<div class="row justify-content-md-center">`;
 
   forecast.forEach(function (forecastDay, index) {
-    if (index < 5) {
+    if (index > 0 && index < 6) {
       forecastHTML =
         forecastHTML +
         `<div class="col col-md-2">
